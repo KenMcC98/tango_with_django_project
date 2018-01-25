5161b0d82c4dc04001f1f7ba6d9b968a70e8c27f
Kenneth McClure <2254721m@student.gla.ac.uk>
2018-01-25 14:43:15 +0000

    Chapter 5 - b.p.2 of exercises

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -16,4 +18,4 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.title
