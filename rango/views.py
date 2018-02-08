128e5fd42fc2680d2cbd00f6b610bac5e8f417a0
Kenneth McClure <2254721m@student.gla.ac.uk>
2018-02-08 16:39:59 +0000

    Chapter 9 - completed

--- rango/views.py
+++ rango/views.py
@@ -160,5 +160,6 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
+    #return HttpResponse("Since you're logged in, you can see this text!")
 
